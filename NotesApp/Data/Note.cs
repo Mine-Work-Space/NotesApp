992b7ea67bf2b7e493391420e682e1d3eeea394f
sashaFPV <andrei.cazanov2017@gmail.com>
2023-09-30 09:21:42 +0300

    Initial commit

---
+++ NotesApp/Data/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace NotesApp.Data
+{
+    public class Note
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+        public DateOnly DateOnly { get; set; }
+    }
+}
