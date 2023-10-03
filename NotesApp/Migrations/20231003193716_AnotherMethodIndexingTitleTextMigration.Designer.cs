550321a4d6522fd773cfec48a3f291d96360a6a0
sashaFPV <andrei.cazanov2017@gmail.com>
2023-10-03 23:21:18 +0300

    feat: searching terms in title & text

--- NotesApp/Migrations/20231003193716_AnotherMethodIndexingTitleTextMigration.Designer.cs
+++ NotesApp/Migrations/20231003193716_AnotherMethodIndexingTitleTextMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NotesApp.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,11 @@ using NpgsqlTypes;
 namespace NotesApp.Migrations
 {
     [DbContext(typeof(DbManager))]
-    partial class DbManagerModelSnapshot : ModelSnapshot
+    [Migration("20231003193716_AnotherMethodIndexingTitleTextMigration")]
+    partial class AnotherMethodIndexingTitleTextMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
