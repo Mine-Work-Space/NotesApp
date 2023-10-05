d7702fcdd678f7f7a42627f04f9a5eb4b6ea6192
sashaFPV <andrei.cazanov2017@gmail.com>
2023-10-05 17:05:15 +0300

    test: NoteRepository

---
+++ NotesApp.Tests/BaseIntegrationTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NotesApp.Data;
+using NotesApp.Repositories.Interfaces;
+
+namespace NotesApp.Tests
+{
+	/// <summary>
+	///     Abstract class used as a base class for integration tests in the application. Implements the 
+	///     <see cref="IClassFixture<IntegrationTestNoteDbFactory>"/> interface, 
+	///     which suggests that it is designed to be used with xUnit.NET for setting up and tearing down resources shared among test classes
+	/// </summary>
+	public abstract class BaseIntegrationTest : IClassFixture<IntegrationTestNoteDbFactory>
+	{
+		private readonly IServiceScope _scope;
+        protected readonly INoteRepository NoteRepository;
+        protected BaseIntegrationTest(IntegrationTestNoteDbFactory factory)
+        {
+            _scope = factory.Services.CreateScope();
+            NoteRepository = _scope.ServiceProvider.GetRequiredService<INoteRepository>();
+        }
+    }
+}
---
+++ NotesApp.Tests/IntegrationTestNoteDbFactory.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NotesApp.Data;
+using Testcontainers.PostgreSql;
+
+namespace NotesApp.Tests
+{
+	/// <summary>
+	///		Used to set up and manage a PostgreSQL database container for integration testing purposes. 
+	/// </summary>
+	public class IntegrationTestNoteDbFactory : WebApplicationFactory<Program>, IAsyncLifetime
+	{
+		private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+			.WithImage("postgres")
+			.WithDatabase("TestNoteDB")
+			.WithPortBinding(5432)
+			.WithUsername("postgres")
+			.WithPassword("postgres")
+			.Build();
+
+		/// <summary>
+		///		Configure test services: Remove the existing DbContextOptions and use DbContext
+		///		with the test connection string.
+		/// </summary>
+		protected override void ConfigureWebHost(IWebHostBuilder builder)
+		{
+			builder.ConfigureTestServices(services =>
+			{
+				var descriptor = services
+					.SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<DbManager>));
+				// Remove DbManager with normal DbContextOptions
+				if (descriptor is not null)
+				{
+					services.Remove(descriptor);
+				}
+				// Adding DbContext with "TestNoteDB" connection string
+				services.AddDbContext<DbManager>(options =>
+				{
+					options.UseNpgsql(_dbContainer.GetConnectionString());
+				});
+			});
+		}
+
+		// Different method
+		public new Task DisposeAsync()
+		{
+			return _dbContainer.StopAsync();
+		}
+
+		public Task InitializeAsync()
+		{
+			return _dbContainer.StartAsync();
+		}
+	} 
+}
